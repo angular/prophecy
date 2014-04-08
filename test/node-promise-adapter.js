696ad86938348c5e281497b398fd299cfebab56e
Jeff Cross <middlefloor@gmail.com>
2014-04-08 09:41:57 -0700

    test(mock): monkey patch promise in a+ adapter to auto-flush queue

--- test/node-promise-adapter.js
+++ test/node-promise-adapter.js
@@ -1,6 +1,30 @@
 var traceurRequire = require('../node_modules/traceur/src/node/require');
 var Promise = traceurRequire('./test/MockPromise.js').MockPromise;
 
+var realInternalResolve = Promise.prototype.internalResolve_;
+Promise.prototype.internalResolve_ = function(value) {
+  realInternalResolve.call(this, value);
+  setTimeout(Promise.flush, 0);
+}
+
+var realInternalReject = Promise.prototype.internalReject_;
+Promise.prototype.internalReject_ = function(value) {
+  realInternalReject.call(this, value);
+  setTimeout(Promise.flush, 0);
+}
+
+var realResolve = Promise.resolve;
+Promise.resolve = function(value) {
+  setTimeout(Promise.flush, 0);
+  return realResolve(value);
+}
+
+var realReject = Promise.reject;
+Promise.reject = function(value) {
+  setTimeout(Promise.flush, 0);
+  return realReject(value);
+}
+
 exports.deferred = function () {
   var resolve, reject;
   var promise = new Promise(function(res, rej) {
